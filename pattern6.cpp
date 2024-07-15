1c51b5dcab11157de93ab39e7f2d9a997b8baebf
chaaruSharma <charu7636@gmail.com>
2024-07-16 00:24:38 +0530

    file upload

--- pattern6.cpp
+++ pattern6.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int count=1;
     for(int i=1; i<=5;i++){
-        for(int j=1;j<=i;j+2){
-        cout<<i;
+        for(int j=1;j<=i;j++){
+        cout<<count<<" ";
+        count++;
         } 
         // cout<<i++;
         cout<<endl;
